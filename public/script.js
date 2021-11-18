86e2c52e88b41e98e33da99360e1a4369eab57dc
Simon <semi.invia@gmail.com>
2021-11-18 15:20:37 +0400

    added lock; edited script;

--- public/script.js
+++ public/script.js
@@ -26,8 +26,8 @@ navigator.mediaDevices.getUserMedia({
 
   socket.on('user-connected', userId => {
     console.log('user connected', userId)
-    connectToNewUser(userId, stream)
-    // setTimeout(connectToNewUser, 1000, userId, stream)
+    // connectToNewUser(userId, stream)
+    setTimeout(connectToNewUser, 1000, userId, stream)
   })
 })
 
